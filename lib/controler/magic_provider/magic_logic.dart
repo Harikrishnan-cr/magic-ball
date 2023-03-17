aa29cf9d881eb8d015092fba4c8ba90007417c91
Harikrishnan-cr <106924175+Harikrishnan-cr@users.noreply.github.com>
2023-03-17 06:08:46 +0530

    hobiee selecter

--- lib/controler/magic_provider/magic_logic.dart
+++ lib/controler/magic_provider/magic_logic.dart
@@ -17,3 +17,68 @@ class MagicCounterclass extends ChangeNotifier {
 
   int get magicCount => _magicCount;
 }
+
+
+
+
+
+
+
+class HobbySelector extends StatefulWidget {
+  final List<String> hobbies;
+  final ValueChanged<List<String>> onChanged;
+
+  HobbySelector({required this.hobbies, required this.onChanged});
+
+  @override
+  _HobbySelectorState createState() => _HobbySelectorState();
+}
+
+class _HobbySelectorState extends State<HobbySelector> {
+  List<String> _selectedHobbies = [];
+
+  void _toggleHobby(String hobby) {
+    setState(() {
+      if (_selectedHobbies.contains(hobby)) {
+        _selectedHobbies.remove(hobby);
+      } else {
+        _selectedHobbies.add(hobby);
+      }
+    });
+    widget.onChanged(_selectedHobbies);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Hobbies:', style: TextStyle(fontSize: 18)),
+        SizedBox(height: 8),
+        Wrap(
+          spacing: 8,
+          children: widget.hobbies
+              .map((hobby) => Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: _selectedHobbies.contains(hobby)
+                          ? Colors.blue
+                          : Colors.grey[300],
+                    ),
+                    child: GestureDetector(
+                      onTap: () => _toggleHobby(hobby),
+                      child: Text(hobby,
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: _selectedHobbies.contains(hobby)
+                                  ? Colors.white
+                                  : Colors.black)),
+                    ),
+                  ))
+              .toList(),
+        ),
+      ],
+    );
+  }
+}
